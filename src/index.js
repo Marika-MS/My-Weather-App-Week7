75f037d67c148789324b35a297bf639cd0d74686
Marika-MS <marika.m.susi@gmail.com>
2024-09-08 23:43:51 -0400

    Integrated forecast with current data

--- src/index.js
+++ src/index.js
@@ -17,7 +17,10 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
-  icon.innerHTML = `<img src="${response.data.condition.icon_url}" class = "weather-app-icon"/>`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class = "weather-app-icon"/>`;
+
+getForecast(response.data.city);
+
 }
 
 function formatDate(date) {
@@ -55,27 +58,49 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-function displayForecast() {
-  
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed","Thu", "Fri", "Sat"];
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[date.getDay()];
+}
+function getForecast(city) {
+    let apiKey = "960t74of2af71309951ecd4291baaaa0";
+    let apiUrl =
+      `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+    
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+
+
+  
   let forecastHtml = "";
 
-  days.forEach(function(day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function(day, index) {
+
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="weather-forecast-day">
-      <div class="weather-forecast-date">${day}</div>
-      <div class="weather-forecast-icon">🌤</div>
+      <div class="weather-forecast-date">${formatDay(day.time)}</div>
+     
+      <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+    
       <div class="weather-forecast-temperatures"> 
       <div class="weather-forecast-temperature">
-      <strong>15°C</strong>
+      <strong>${Math.round(day.temperature.maximum)}°</strong>
       </div>
-    <div class="weather-forecast-temperature">9°C</div>  
+    <div class="weather-forecast-temperature">${Math.round(
+      day.temperature.minimum
+    )}°</div>  
     </div>
     </div>
     `;
+    }
   });
 let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
@@ -85,4 +110,4 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Zhangjiajie");
-displayForecast();
+getForecast ("Zhangjiajie");
